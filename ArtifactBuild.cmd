369eec7ac555baef1c09d768ea4929e016f00453
Jace <54625750+Jacekun@users.noreply.github.com>
2021-11-28 09:51:53 +0800

    changed build release file

--- ArtifactBuild.cmd
+++ ArtifactBuild.cmd
@@ -1,5 +1,5 @@
 @echo off
 pushd "%~dp0"
-powershell Compress-7Zip "Release" -ArchiveFileName "SampleAnyCPU.zip" -Format Zip
+powershell Compress-7Zip "CubariHelper\bin\Release" -ArchiveFileName "CubariHelper_AnyCPU.zip" -Format Zip
 :exit
 popd
