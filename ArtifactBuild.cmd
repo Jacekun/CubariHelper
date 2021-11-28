9eac42f407e5109ab5b6e64e9edc888c7e44a19e
Jace <54625750+Jacekun@users.noreply.github.com>
2021-11-28 10:15:48 +0800

    build on nested net5.0-windows

--- ArtifactBuild.cmd
+++ ArtifactBuild.cmd
@@ -1,5 +1,5 @@
 @echo off
 pushd "%~dp0"
-powershell Compress-7Zip "CubariHelper\bin\Release" -ArchiveFileName "CubariHelper_AnyCPU.zip" -Format Zip
+powershell Compress-7Zip "CubariHelper\bin\Release\net5.0-windows" -ArchiveFileName "CubariHelper_AnyCPU.zip" -Format Zip
 :exit
 popd
